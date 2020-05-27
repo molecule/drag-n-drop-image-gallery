c71acf4ab935d35a3101f6529269ccaa8ddcb507
Molly Nicholas <molecule@berkeley.edu>
2020-05-27 11:59:24 -0700

    basic layout from react grid layout resizable, draggable, etc works but all teh boxes are white so impossible to see

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './styles.css';
 import App from './App';
+import BasicLayout from './react-grid-layout';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BasicLayout />
   </React.StrictMode>,
   document.getElementById('root')
 );
