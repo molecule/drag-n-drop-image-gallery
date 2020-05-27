76e69f1f94c1a41673bcc69cd919a5dcc22add92
Molly Nicholas <molecule@berkeley.edu>
2020-05-27 14:37:40 -0700

    remove img for working demo

--- src/react-grid-layout.js
+++ src/react-grid-layout.js
@@ -31,7 +31,6 @@ export default class BasicLayout extends React.PureComponent {
     return _.map(_.range(this.props.items), function(i) {
       return (
         <div key={i}>
-          <img src="https://i.imgur.com/BLEYNX6.png" />
           <span className="text">{i+100}</span>
         </div>
       );
